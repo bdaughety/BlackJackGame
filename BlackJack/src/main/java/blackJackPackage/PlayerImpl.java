275206ec87967856ce8736a3e09383e61929f595
bdaughety <bdaughety@franklinamerican.com>
2015-08-12 21:29:53 -0500

    getter and setters for PlayerImpl.java

--- BlackJack/src/main/java/blackJackPackage/PlayerImpl.java
+++ BlackJack/src/main/java/blackJackPackage/PlayerImpl.java
@@ -4,102 +4,128 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerImpl implements Player {
-    private Integer score;
-    private Integer handSize;
-    private List<CardEnum> playerHand;
-    private boolean isStanding = false;
-    private boolean blackJack;
-    private boolean busted;
+	private Integer score;
+	private Integer handSize;
+	private List<CardEnum> playerHand;
+	private boolean isStanding = false;
+	private boolean blackJack;
+	private boolean busted;
 
-    public void hit(CardEnum newCard) {
-        if (playerHand == null || playerHand.isEmpty()) {
-            playerHand = new ArrayList<CardEnum>();
-        }
-        if (!isStanding && !hasBlackJack() && !hasBusted()) {
-            playerHand.add(newCard);
-        }
-        calculateScore();
-        hasBlackJack();
-    }
+	public Integer calculateScore() {
+		score = 0;
+		if (playerHand != null && !playerHand.isEmpty()) {
+			for (CardEnum cardEnum : playerHand) {
+				score += cardEnum.blackJackValue();
+				hasBusted();
+				hasBlackJack();
+			}
+		}
+		return score;
+	}
 
-    public void stand() {
-        isStanding = true;
-    }
+	public Integer getHandSize() {
+		return handSize;
+	}
 
-    public boolean isStanding() {
-        return isStanding;
-    }
+	public List<CardEnum> getPlayerHand() {
+		if (playerHand == null || playerHand.isEmpty()) {
+			setPlayerHand(new ArrayList<CardEnum>());
+		}
+		return playerHand;
+	}
 
-    public Integer calculateScore() {
-        score = 0;
-        if (playerHand != null && !playerHand.isEmpty()) {
-            for (CardEnum cardEnum : playerHand) {
-                score += cardEnum.blackJackValue();
-                hasBusted();
-                hasBlackJack();
-            }
-        }
-        return score;
-    }
+	public Integer getScore() {
+		return score;
+	}
 
-    public boolean hasBlackJack() {
-        blackJack = false;
-        if (playerHand != null && playerHand.size() == 2) {
-            if (score.equals(BLACK_JACK_SCORE)) {
-                blackJack = true;
-                stand();
-            }
-        }
-        return blackJack;
-    }
+	public boolean handContainsAnAce() {
+		return getPlayerHand().contains(CardEnum.ACE_OF_SPADES)
+			   || getPlayerHand().contains(CardEnum.ACE_OF_CLUBS)
+			   || getPlayerHand().contains(CardEnum.ACE_OF_DIAMONDS)
+			   || getPlayerHand().contains(CardEnum.ACE_OF_HEARTS);
+	}
 
-    public void resetHand() {
-        if (playerHand != null && !playerHand.isEmpty()) {
-            playerHand.clear();
-        }
-        calculateScore();
-        isStanding = false;
-    }
+	public boolean hasBlackJack() {
+		setBlackJack(false);
+		if (playerHand != null && playerHand.size() == 2) {
+			if (score.equals(BLACK_JACK_SCORE)) {
+				setBlackJack(true);
+				stand();
+			}
+		}
+		return isBlackJack();
+	}
 
-    public boolean hasBusted() {
-        busted = false;
-        calculateScore();
-        if (score > BLACK_JACK_SCORE) {
-            if ((handContainsAnAce() && score > PERFECT_SCORE_WITH_ACE)
-                    || !handContainsAnAce()) {
-                busted = true;
-                stand();
-            }
-        }
-        return busted;
-    }
+	public boolean hasBusted() {
+		setBusted(false);
+		calculateScore();
+		if (score > BLACK_JACK_SCORE) {
+			if ((handContainsAnAce() && score > PERFECT_SCORE_WITH_ACE)
+				|| !handContainsAnAce()) {
+				setBusted(true);
+				stand();
+			}
+		}
+		return isBusted();
+	}
 
-    public boolean handContainsAnAce() {
-        for (CardEnum cardEnum : getPlayerHand()) {
-            if (cardEnum.cardName().equals(CardEnum.ACE_OF_CLUBS.cardName())) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public void hit(CardEnum newCard) {
+		if (playerHand == null || playerHand.isEmpty()) {
+			playerHand = new ArrayList<CardEnum>();
+		}
+		if (!isStanding && !hasBlackJack() && !hasBusted()) {
+			playerHand.add(newCard);
+		}
+		calculateScore();
+		hasBlackJack();
+	}
 
-    public Integer getScore() {
-        return score;
-    }
+	public boolean isBlackJack() {
+		return blackJack;
+	}
 
-    public Integer getHandSize() {
-        handSize = 0;
-        if (playerHand != null) {
-            handSize = playerHand.size();
-        }
-        return handSize;
-    }
+	public boolean isBusted() {
+		return busted;
+	}
 
-    public List<CardEnum> getPlayerHand() {
-        if (playerHand == null || playerHand.isEmpty()) {
-            playerHand = new ArrayList<CardEnum>();
-        }
-        return playerHand;
-    }
+	public boolean isStanding() {
+		return isStanding;
+	}
+
+	public void resetHand() {
+		if (playerHand != null && !playerHand.isEmpty()) {
+			playerHand.clear();
+		}
+		calculateScore();
+		setIsStanding(false);
+	}
+
+	public void setBlackJack(final boolean blackJack) {
+		this.blackJack = blackJack;
+	}
+
+	public void setBusted(final boolean busted) {
+		this.busted = busted;
+	}
+
+	public void setHandSize(final Integer handSize) {
+		this.handSize = handSize;
+	}
+
+	public void setIsStanding(final boolean isStanding) {
+		this.isStanding = isStanding;
+	}
+
+	public void setPlayerHand(final List<CardEnum> playerHand) {
+		this.playerHand = playerHand;
+	}
+
+	public void setScore(final Integer score) {
+		this.score = score;
+	}
+
+	public void stand() {
+		isStanding = true;
+	}
 
 }
